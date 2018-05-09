50d51bc0135c7c6bb960a598a75b9e6d54df76e1
Thomaz Diniz <thomaz_diniz@hotmail.com>
2018-05-09 20:25:46 -0300

    Movimentação do avatar inicial

--- minario.c
+++ minario.c
@@ -25,7 +25,11 @@
 void limpaTela(){
 	system("cls");
 }
-
+//Manda o teclado para a coordenada desejada
+void cursorSetPos(int x, int y){
+	COORD p = {x,y}; //Estrutura de coordenadas?
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),p);//muda a coordenada do cursor para a coordenada p
+}
 void imprimeIntroducao(){
 	limpaTela();
 	printf("\n\t/////////////////////////////////\tMinário\t/////////////////////////////////");
@@ -54,13 +58,16 @@ bool pressESQ = false;
 bool pressDIR = false;
 bool pressCIM = false;
 bool pressBAI = false;
-
 bool pressESC = false;
 bool pressENTER = false;
 
+char pressingESQ = 0;
+char pressingDIR = 0;
+char pressingCIM = 0;
+char pressingBAI = 0;
 
 //Checagem de inputs
-bool atualizaBotoes(){	
+bool atualizaBotoes(){
 	pressESQ = false;
 	pressDIR = false;
 	pressCIM = false;
@@ -75,31 +82,19 @@ bool atualizaBotoes(){
 		switch(botao){
 			case SETAS:
 				botao = getch();
-				case ESQUERDA:
-					pressESQ = true;
-				break;
-					
-				case DIREITA:
-					pressDIR = true;
-				break;
-				
-				case CIMA:
-					pressCIM = true;
-				break;
-				
-				case BAIXO:
-					pressBAI = true;
-				break;
+				case ESQUERDA: 	pressESQ = true; 	break;
+				case DIREITA:	pressDIR = true;	break;
+				case CIMA:		pressCIM = true;	break;
+				case BAIXO:		pressBAI = true;	break;
 			break;
-				
-			case ESC:
-				pressESC = true;
-			break;				
 			
-			case ENTER:
-				pressENTER = true;
-			break;	
-
+			case 'a':	case 'A':	pressESQ = true;	break;
+			case 'w': 	case 'W':	pressCIM = true;	break;
+			case 's': 	case 'S':	pressBAI = true;	break;
+			case 'd':	case 'D':	pressDIR = true;	break;
+			
+			case ESC:	pressESC = true;	break;				
+			case ENTER:	pressENTER = true;	break;	
 			default:
 				return false;
 			break;
@@ -110,11 +105,23 @@ bool atualizaBotoes(){
 }
 
 int main(){
-	//Muda o jogo para porugues
 	setlocale(LC_ALL, "Portuguese");
-	int estado = 0;
+	int estado = ESTADO_IMPRIME_MENU;
+	int vx = 0;
+	int vy = 0;
+	int x = 4;
+	int y = 4;
+	int xPrevious = -1;
+	int yPrevious = -1;
 	while(1){
 		atualizaBotoes();
+		vx = 0;
+		vy = 0;
+		if (pressESQ){vx=-1;vy = 0;}
+		if (pressDIR){vx=1;vy = 0;}
+		if (pressCIM){vy=-1;vx = 0;}
+		if (pressBAI){vy=1;vx = 0;}
+		if (pressESC){break;}
 		switch(estado){
 			case ESTADO_IMPRIME_MENU:
 				imprimeIntroducao();
@@ -124,15 +131,25 @@ int main(){
 				if (pressENTER){limpaTela(); estado++;}
 			break;
 			case ESTADO_INICIA_JOGO:
+				Sleep(60);
+				x+=vx;
+				y+=vy;
+				if (x < 0){x = 0;}
+				if (y < 0){y = 0;}
+				if (x > 50){x = 50;}
+				if (y > 50){y = 50;}
+				if (x != xPrevious || y != yPrevious){
+					cursorSetPos(xPrevious,yPrevious);
+						printf("   ");
+					cursorSetPos(x,y);
+						printf("^.^");
+					xPrevious = x;
+					yPrevious = y;
+				}
 			break;
 			case ESTADO_JOGO:
 			break;
 		}	
-		if (pressESQ){printf("Tecla Esquerda Pressionada");}
-		if (pressDIR){printf("Tecla DIREITA Pressionada");}
-		if (pressCIM){printf("Tecla CIMA Pressionada");}
-		if (pressBAI){printf("Tecla BAIXO Pressionada");}
-		if (pressESC){break;}
 	}
 	printf("Fim de jogo");
 	return 0;
