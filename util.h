13f5b5657786844f64020e3e575161b078c8f972
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-06-01 00:52:08 -0300

    Removing unnecessary code

--- util.h
+++ util.h
@@ -18,7 +18,6 @@ long long getCurrentTimestamp() {
     return milliseconds;
 }
 
-
 int keyboardHit() {
     int ch = getch();
     if (ch != ERR) {
@@ -31,11 +30,4 @@ int keyboardHit() {
 
 int getRandomInteger(int i){
 	return rand() % i+1;
-}
-
-bool chance(int i){
-	//Returns true if a given chance has happened.
-	//A chance is determined by the first parameter, so for exemple chance(2) has a 50% of returning true
-	//chance(3) has a 33.3% of returning true and so on.
-	return getRandomInteger(i) == i;
 }
