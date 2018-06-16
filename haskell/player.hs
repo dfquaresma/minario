60ec1081eb26c816fcfcf6eaee35dce673b25a29
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2018-06-16 13:39:42 -0300

    Implement viewChance and move update for bots.

--- haskell/player.hs
+++ haskell/player.hs
@@ -1,9 +1,10 @@
 module Players ( 
     buildPlayer,
     createPlayers,
-    getNewBotsState,
-    checkSafePosition,
-    updatePlayerState 
+    safeByView,
+    checkSafeByViewPosition,
+    newBotPosition,
+    updateBotPosition
 ) where 
 
 import Util (getRandomInteger)
@@ -14,7 +15,7 @@ xMin = 0
 xMax = 10
 yMin = 0
 yMax = 10
--- return a player.
+-- It returns a player.
 buildPlayer :: Player
 buildPlayer = newPlayer
                 where 
@@ -23,22 +24,50 @@ buildPlayer = newPlayer
                     isAlive = True
                     newPlayer = Player xPosition yPosition isAlive 
                 
--- return a list of players.
+-- It returns a list of players.
 createPlayers :: Int -> [Player]
 createPlayers 0 = []
 createPlayers n = buildPlayer : createPlayers (n - 1)
 
--- to be implemented yet.
--- TODO(David)
-checkSafePosition :: [Player] -> (Int, Int) -> Bool 
-checkSafePosition bots position = False
+-- It checks if is a safe position considering view chance.
+viewChance = 2
+safeByView :: (Int, Int) -> (Int, Int) -> Bool
+safeByView (x1, y1) (x2, y2) =  if viewChance == viewChance -- getRandomInteger (1, viewChance) 
+                                    then x1 /= x2 || y1 /= y2 
+                                else True
 
--- to be implemented yet.
--- TODO(David)
-getNewBotsState :: [Player] -> [Player]
-getNewBotsState bots = []
-                    
--- to be implemented yet.
--- TODO(David)
+-- It checks if the given position is safe compared with (may) all other bots positions.
+checkSafeByViewPosition :: [Player] -> (Int, Int) -> Bool 
+checkSafeByViewPosition [] pos = True
+checkSafeByViewPosition (headBot:tailBots) (xPos, yPos) = safeByView (xPos, yPos) (xPosition headBot, yPosition headBot) 
+                                                  && checkSafeByViewPosition tailBots (xPos, yPos)
+
+-- It gives a new positions given an old one.
+botMovementRange = (-1, 1) 
+newBotPosition :: (Int, Int) -> (Int, Int)
+newBotPosition (xPos, yPos) = (xPos + 0, yPos + 0) -- (xPos + getRandomInteger botMovementRange, yPos + getRandomInteger botMovementRange)                                                  
+
+-- It gives a new bots distribution list. Note that it does not ensures that colliding bots are dead. 
+updateBotPosition :: [Player] -> [Player]
+updateBotPosition [] = []
+updateBotPosition (headBot:bots) =  if isAlive headBot then 
+                                        if checkSafeByViewPosition bots newPos then 
+                                            Player (fst newPos) (snd newPos) True : updateBotPosition bots
+                                        else
+                                            updateBotPosition (headBot:bots) -- try again a newPos.
+
+                                    else 
+                                        headBot : updateBotPosition bots
+                                  
+                                    where newPos = newBotPosition (xPosition headBot, yPosition headBot)
+               
+-- It verifies if there is any alive players collinding with each other or with a dead player or with the board and kill them.
+-- TODO (David)                                         
+updateDeadBots :: [Player] -> [Player]
+updateDeadBots [] = []
+updateDeadBots (headBot:bots) = updateDeadBots bots
+                                    
+-- It returns a list with the player and all bots.
+-- TODO(Renato)
 updatePlayerState :: [Player] -> (Int, Int) -> [Player]
-updatePlayerState playerAndBots position = []
+updatePlayerState (player:bots) (xPos, yPos) = [] 
