c6903de5de5b2cdbf6f280792b59537f4004754f
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2018-06-27 00:02:24 -0300

    Initial implementation of main able to read input. Only work on linux.

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,7 +1,32 @@
+import Display
 
+import Control.Concurrent
+import Control.Monad
+import Data.Maybe
+import System.IO
 
-import Players (Player)
+wow :: IO()
+wow = do
+    c <- newEmptyMVar 
+    hSetBuffering stdin NoBuffering
+    hSetEcho stdin False
+    forkIO $ do
+        a <- getChar
+        putMVar c a
+        case a of
+            's' -> showGameIntroductionStaticInstructions
+            'w' -> showMainGameStaticStart 
+            otherwise -> return () 
+    
+    wait c
+    where wait c = do
+          a <- tryTakeMVar c
+          if isJust a then wow
+          else 
+               threadDelay 500 >> wait c
+
 
 main :: IO()
 main = do
-    putStrLn ""
+    showMainGameIntroduction
+    wow
