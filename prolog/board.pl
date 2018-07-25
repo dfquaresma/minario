2794a977452459bed89cc72f376cc58f200f64f7
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2018-07-25 01:28:24 -0300

    Update to draw players and bots. It also start reduction implementation.

--- prolog/board.pl
+++ prolog/board.pl
@@ -1,37 +1,56 @@
-:- initialization (main).
+:- module(
+    'board', 
+    [drawGameBoard/0,
+	deleteWallSize/1,
+	setWallSize/1,
+	isCollidingWithBoard/2
+	]
+).
+:- use_module(players).
+
+wallSize(1).
+
+deleteWallSize(W) :- 
+	retract(wallSize(W)).
+
+setWallSize(W) :- 
+	assertz(wallSize(W)).
+
+isCollidingWithBoard(X, Y) :- 
+	wallSize(W) -> (
+		X =< W; 
+		X >= (70 - W);
+		Y =< W; 
+		Y >= (30 - W)
+	).
 
 drawGameBoard() :-
-	drawBoard(70,30,0,1,[],[]).
+	wallSize(W),
+	drawBoard(70, 30, 0, W).
 
-drawBoard(Width,Height,Row,WallSize,Player,Bots) :-
+drawBoard(Width,Height,Row,WallSize) :-
 	Row < Height,
-		drawRow(Width,Height,Row,0,WallSize,Player,Bots),nl,
+		drawRow(Width,Height,Row,0,WallSize),nl,
 		NewRow is (Row + 1),
-		drawBoard(Width,Height,NewRow,WallSize,Player,Bots)
+		drawBoard(Width,Height,NewRow,WallSize)
 	;	true.
 
-drawRow(Width,Height,Row,Col,WallSize,Player,Bots) :-
+drawRow(Width,Height,Row,Col,WallSize) :-
 	Col < Width,
-		drawPosition(Width,Height,Row,Col,WallSize,Player,Bots),
+		drawPosition(Width,Height,Row,Col,WallSize),
 		NewCol is (Col + 1),
-		drawRow(Width,Height,Row,NewCol,WallSize,Player,Bots)
+		drawRow(Width,Height,Row,NewCol,WallSize)
 	;	
 		true.
 
-drawPosition(Width,Height,Row,Col,WallSize,Player,Bots) :-
-	isPlayerPosition(Row,Col,Player),
+drawPosition(Width,Height,Row,Col,WallSize) :-
+	isPlayerPosition(Row,Col),
 		write('O')
-	; isBotPosition(Row,Col,Bots),
+	; isBotPosition(Row,Col),
 		write('X')
 	; isWallPosition(Width,Height,Row,Col,WallSize),
 		write('#')
 	;	write(' ').
-
-isPlayerPosition(Row,Col,Player) :-
-	false.
-	
-isBotPosition(Row,Col,Bots) :-
-	false.
 
 isWallPosition(Width,Height,Row,Col,WallSize) :-
 	Row =< WallSize,
@@ -44,8 +63,3 @@ isWallPosition(Width,Height,Row,Col,WallSize) :-
 		true
 	;
 		false.
-
-
-
-main :-
-	drawGameBoard().
