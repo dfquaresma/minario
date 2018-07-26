a88c4b7072bc950a283bb3943665040da9cdbcec
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2018-07-26 03:30:39 -0300

    Add board reduction logic.

--- prolog/main.pl
+++ prolog/main.pl
@@ -47,33 +47,60 @@ getUserAction() :-
 	get_single_char(UserAction),
 	applyUserAction(UserAction).
 
+updateGameScreen() :-
+	drawGameBoard(), 
+	getNumberOfPlayers(PlayersAlive),  
+	write("Players alive: "), writeln(PlayersAlive),
+	write("Player lives? "),
+	isPlayerAlive() -> (
+		writeln("YES!")
+	);
+	writeln("YES!").
+
 applyUserAction(27) :- 
 	retract(quitGame(0)), 
 	asserta(quitGame(1)),
 	writeln("Leaving Game...").
 applyUserAction(UserAction) :-
-	movement(UserAction, XVar, YVar),
-	updatePlayerPosition(XVar, YVar),
-	getUserAction();
-	getUserAction().
+	isPlayerAlive() -> (movement(UserAction, XVar, YVar),
+		updatePlayerPosition(XVar, YVar),
+		updateGameScreen(), 
+		getUserAction();
+		getUserAction()
+	);
+	retract(quitGame(0)), 
+	asserta(quitGame(1)).
 
-numberOfPlayers(50).
+boardReduction(0, _) :- 
+	retract(endGame(0)), 
+	asserta(endGame(1)).
+boardReduction(N, W) :-
+	sleep(1.4),
+	deleteWallSize(W), 
+	NewW is W + 1,
+	setWallSize(NewW), 
+	updateGameScreen(),
+	NewN is N - 1,
+	boardReduction(NewN, NewW).
 
 gameLoop() :- 
 	endGame(1);
 	endGame(0),
-	sleep(0.5),
-	numberOfPlayers(NumberOfPlayers), updateBotsPosition(NumberOfPlayers), 
-	drawGameBoard(),
+	updateGameScreen(), 
+	sleep(0.2),
+	updateBotsPosition(30), 
 	gameLoop().
 
 gameSetup() :-
 	asserta(endGame(0)),
+	setWallSize(0), 
 	thread_create(getUserAction(), UserThreadId),
-	numberOfPlayers(NumberOfPlayers), 
-	buildPlayers(NumberOfPlayers),
+	thread_create(boardReduction(5, 0), BoardThreadId),
+	buildPlayer(),
+	buildBots(29), 
 	gameLoop(),
-	thread_join(UserThreadId).
+	thread_join(UserThreadId),
+	thread_join(BoardThreadId).
 
 main :-
 	introduction(0),
